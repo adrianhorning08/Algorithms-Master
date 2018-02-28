59dd5937342be5ec218f388d73afb12d26183724
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-27 22:25:53 -0800

    done

--- adrian_horning_algo_2/lib/p04_linked_list.rb
+++ adrian_horning_algo_2/lib/p04_linked_list.rb
@@ -15,11 +15,16 @@ class Node
   def remove
     # optional but useful, connects previous node to next node
     # and removes self from list.
+    @prev = @next
   end
 end
 
 class LinkedList
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,24 +33,49 @@ class LinkedList
   end
 
   def first
+    return nil if empty?
+    @head.next
   end
 
   def last
+    return nil if empty?
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    while @head.next.key != @tail
+      return @head.next.val if @head.next.key == key
+      @head.next
+    end
+    false
   end
 
   def include?(key)
+    while @head.next.key != @tail
+      return true if @head.next.key == key
+      @head.next
+    end
+    false
   end
 
   def append(key, val)
+    node = Node.new(key,val)
+    @tail.next = node
+    @tail = node
   end
 
   def update(key, val)
+    each { |link| return link.val = val if link.key == key }
+    new_link = Link.new(key, val)
+    @tail.prev.next = new_link
+    new_link.prev = @tail.prev
+    new_link.next = @tail
+    @tail.prev = new_link
+    new_link
   end
 
   def remove(key)
