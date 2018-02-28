f2aef4d5c3b38705ed4256231e1f06ca7aea4c5c
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-27 21:05:47 -0800

    hashing done

--- adrian_horning_algo_2/lib/p02_hashing.rb
+++ adrian_horning_algo_2/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    result = 0
+    self.each_with_index do |el, i|
+      result += el.hash * i
+    end
+    result
   end
 end
 
 class String
   def hash
+    result = 0
+    i = 0
+    while i < self.length
+      result += self[i].ord * i
+      i +=1
+    end
+    result
   end
 end
 
@@ -16,6 +28,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    result = 0
+    self.each do |key, val|
+      result += self[key].ord * val .ord
+    end
+    result
   end
 end
