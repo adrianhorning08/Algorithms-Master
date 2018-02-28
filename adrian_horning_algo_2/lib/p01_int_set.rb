88178f3fff2fbcbd7be98136b48599c2d0da20a3
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-27 20:45:03 -0800

    part 1 done

--- adrian_horning_algo_2/lib/p01_int_set.rb
+++ adrian_horning_algo_2/lib/p01_int_set.rb
@@ -1,23 +1,34 @@
 class MaxIntSet
   def initialize(max)
-
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    validate!(num)
+    return false if @store[num]
+    @store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    return nil unless include?(num)
+    @store[num] = false
+    num
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num.between?(0, @store.length - 1)
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
