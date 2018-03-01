377bda16d0c9a95cf7aa6c3409def26590aa808d
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-28 20:51:49 -0800

    heapify done

--- adrian_horning_heaps/lib/heap.rb
+++ adrian_horning_heaps/lib/heap.rb
@@ -3,6 +3,7 @@ class BinaryMinHeap
 
   def initialize(&prc)
     @store = []
+    @prc = prc || Proc.new{|a,b| a<=>b}
   end
 
   def count
@@ -19,6 +20,10 @@ class BinaryMinHeap
   end
 
   public
+  def self.swap!(array, i1, i2)
+   array[i1], array[i2] = array[i2], array[i1]
+ end
+
   def self.child_indices(len, parent_idx)
     parent_idx_times_two = parent_idx * 2
     if parent_idx_times_two + 2 == len
@@ -34,8 +39,42 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
-  end
+      prc ||= Proc.new { |a, b| a <=> b }
+      return self if len <= 1
+      while parent_idx <= parent_index(len-1)
+        children = child_indices(len, parent_idx)
+        if children.length == 1
+          smallest_child_idx = children[0]
+        else
+          child1_idx, child2_idx = children
+          if prc.call(array[child1_idx], array[child2_idx]) <= 0
+            smallest_child_idx = child1_idx
+          else
+            smallest_child_idx = child2_idx
+          end
+        end
+
+        if prc.call(array[parent_idx], array[smallest_child_idx]) > 0
+          swap!(array, parent_idx, smallest_child_idx)
+          parent_idx = smallest_child_idx
+        else
+          break
+        end
+      end
+      array
+    end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
+    while child_idx > 0
+      parent_idx = parent_index(child_idx)
+      if prc.call(array[parent_idx], array[child_idx]) > 0
+        swap!(array, parent_idx, child_idx)
+        child_idx = parent_idx
+      else
+        break
+      end
+    end
+    array
   end
 end
