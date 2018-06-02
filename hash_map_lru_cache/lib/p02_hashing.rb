7c10a96965d4603ac5caff70588476757aadac3f
Adrian Horning <m.adrian.horning@gmail.com>
2018-06-02 12:16:58 -0700

    Done with hashing

--- hash_map_lru_cache/lib/p02_hashing.rb
+++ hash_map_lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
+    each_with_index.inject(0) do |intermediate_hash, (el, i)|
+      (el.hash + i.hash) ^ intermediate_hash
+    end
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.split('').each_with_index do |el, i|
+      sum += (el.ord * i)
+    end
+    sum
   end
 end
 
@@ -16,6 +24,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort_by(&:hash).hash
   end
 end
