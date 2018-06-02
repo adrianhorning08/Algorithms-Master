8b2264b28d9b0b9a4b49aa0a6a498eeb18b94e94
Adrian Horning <m.adrian.horning@gmail.com>
2018-06-02 15:05:19 -0700

    Linked List done

--- hash_map_lru_cache/lib/p04_linked_list.rb
+++ hash_map_lru_cache/lib/p04_linked_list.rb
@@ -14,7 +14,13 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -23,31 +29,61 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : @head.next
   end
 
   def last
+    empty? ? nil : @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
+    nil
   end
 
   def include?(key)
+    any? { |link| link.key == key }
   end
 
   def insert(key, val)
+    each { |link| return link.val = val if link.key == key }
+
+    new_link = Link.new(key, val)
+
+    @tail.prev.next = new_link
+    new_link.prev = @tail.prev
+    new_link.next = @tail
+    @tail.prev = new_link
+
+    new_link
   end
 
   def remove(key)
+    each do |link|
+      if link.key == key
+        link.prev.next = link.next
+        link.next.prev = link.prev
+        link.next, link.prev = nil, nil
+        return link.val
+      end
+    end
+
+    nil
   end
 
   def each
+    current_link = @head.next
+    until current_link == @tail
+      yield current_link
+      current_link = current_link.next
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
