b71cbd7bce320a42b5d451382456a9049665d230
Adrian Horning <m.adrian.horning@gmail.com>
2018-06-05 10:43:50 -0700

    perms

--- hash_map_lru_cache/lib/p06_lru_cache.rb
+++ hash_map_lru_cache/lib/p06_lru_cache.rb
@@ -15,6 +15,12 @@ class LRUCache
   end
 
   def get(key)
+    if @map[key]
+      # move it to the back
+      update_link!(@map[key])
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -25,12 +31,20 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = prc.call(key)
+    new_link = @store.insert(key, val)
+    @map[key] = new_link
   end
 
   def update_link!(link)
     # suggested helper method; move a link to the end of the list
+    new_link = @store.get(link.key)
+    new_link.next = nil
+    new_link.prev = @store.last
+    @store.
   end
 
   def eject!
+    # how can you eject a key if you don't pass it in as an arg?
   end
 end
